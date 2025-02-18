dbec59a627aeb13670d75d220d7f0247eb71213e
JiaQuan07 <lquan051011@gmail.com>
2025-02-18 16:55:35 +0800

    Testing

---
+++ Lab/Lab1/Q2.java
@@ -0,0 +1,8 @@
+public class Q2 {
+    public static void main(String[] args) {
+        System.out.println("Name : JiaQuan07");
+        System.out.println("Matric Number : 2400XXXX");
+        System.out.println("Address Email : 2400XXXX@siswa.um.edu.my");
+        System.out.println("Contact : 011XXXXXXXX");
+    }
+}
